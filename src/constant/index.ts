1b33339d093dbee6de4aaa79f22ca9ab68597238
d0nda <dondahq@gmail.com>
2024-01-18 22:58:44 +0200

    added constant/index.ts( category filter)

---
+++ src/constant/index.ts
@@ -0,0 +1,8 @@
+export const categoryFilter = ['Academic', 'Accounting', 'Administration', 'Advertising', 'Agriculture', 'Automotive', 'Aviation',
+    'Banking', 'Business_Management', 'Call_Centre', 'Chemical', 'Clothing', 'Construction', 'Consulting', 'Cruise_Ship', 'Defence', 'Design_Services',
+    'Ecommerce', 'Education', 'Engineering', 'Entertainment', 'Environmental', 'FMCG', 'Fashion', 'Finance', 'General_Employment', 'Government',
+    'Graduate', 'Health_and_Safety', 'Hospitality', 'Human_Resources', 'IT_and_Computer', 'Import_and_Export', 'Insurance', 'Investment', 'Legal',
+    'Lifestyle', 'Logistics', 'Management', 'Manufacturing', 'Market_Research', 'Marketing', 'Media', 'Medical', 'Mining', 'Motoring', 'NGOs', 'PR_and_Communication',
+    'Petrochemical', 'Pharmaceutical', 'Property', 'Purchasing', 'Real_Estate', 'Recruitment', 'Research', 'Retail', 'Sales', 'Social_Services',
+    'Soft_Skills', 'Sports', 'Stockbroking', 'Technical', 'Technology', 'Telecommunications', 'Trades_and_Services', 'Travel_and_Tourism',
+    'Warehousing']
