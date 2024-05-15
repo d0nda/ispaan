f7b09bb9a33171b5f1ac4040c578c22fedefc85d
d0nda <dondahq@gmail.com>
2024-05-15 17:42:17 +0200

    entry page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-md sm:text-center">
             <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-black dark:text-white sm:text-4x">Sign up for our newsletter</h2>
-            <p className="mx-auto mb-8 max-w-2xl font-light text-black dark:text-white md:mb-12 sm:text-xl">Stay up to date with the announcements and exclusive discounts feel free to sign up with your email.</p>
+            <p className="mx-auto mb-8 max-w-2xl font-light text-black dark:text-white md:mb-12 sm:text-xl">Stay up to date with the Latest and Newly posted jobs feel free to sign up with your email.</p>
             <form action="#">
               <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                 <div className="relative w-full">
@@ -83,7 +83,7 @@ export default function Home() {
                   <input className="block p-3 pl-10 w-full text-sm text-gray-400 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" required />
                 </div>
                 <div>
-                  <button type="submit" className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg bg-indigo-500 border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
+                  <button type="submit" className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg bg-indigo-300 border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
                 </div>
               </div>
             </form>
