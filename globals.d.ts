38d9daabeb0a70c952934726bfbe4f9d4858e2b9
d0nda <dondahq@gmail.com>
2024-07-10 10:52:34 +0200

    prisma removal

--- globals.d.ts
+++ globals.d.ts
@@ -1,12 +1,6 @@
 declare global {
     namespace NodeJS {
       interface ProcessEnv {
-        DATABASE_URL: string;
-        NEXTAUTH_SECRET: string;
-        NEXTAUTH_URL: string;
-        GOOGLE_CLIENT_ID: string;
-        GOOGLE_CLIENT_SECRET: string;
-        API_URL: string;
         RAPID_API_KEY: string;
       }
     }
--- src/app/about/about.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function about() {
-    return (
-        <h1>About us info Here</h1>
-    )
-}
