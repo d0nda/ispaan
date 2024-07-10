36dfe47711c5f23b855dc5506a40815969ef18a6
d0nda <dondahq@gmail.com>
2024-07-10 10:57:02 +0200

    ...

--- next.config.js
+++ next.config.js
@@ -1,28 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/a/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
-                port: '',
-                pathname: '/u/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'github.com',
-                port: '',
-                pathname: '/8thwanda/screenshots/blob/main/screenshot/**',
-            },
-        ],
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
